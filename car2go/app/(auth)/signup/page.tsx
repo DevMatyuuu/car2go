73129d3648037310bd63bd540e867b2109c946e6
Ross Matthew Santos <matthewsantos120216@gmail.com>
2024-03-04 18:30:37 +0800

    fixed signup page mobile responsiveness

--- car2go/app/(auth)/signup/page.tsx
+++ car2go/app/(auth)/signup/page.tsx
@@ -65,24 +65,24 @@ export default function SignUp() {
   }
 
   return (
-      <div className='flex justify-center items-center h-screen'>
-        <div className='flex h-[500px] xl:h-[650px] lg:h-[500px] md:h-[500px] shadow-2xl rounded-xl'>
-          <form onSubmit={handleSubmit} className='flex flex-col justify-center w-[400px] lg:w-[550px] md:w-[350px] items-center py-12 rounded-xl md:rounded-r-none'>
-            <Image src={logo} alt='devoro-logo' className='h-24 w-28 mb-10' priority/>
+      <div className='flex justify-center items-center h-screen bg-primary xl:bg-white'>
+        <div className='flex h-[600px] xl:h-[650px] lg:h-[500px] md:h-[500px] shadow-2xl rounded-xl bg-white'>
+          <form onSubmit={handleSubmit} className='flex flex-col justify-center w-[400px] xl:w-[550px] md:w-[350px] items-center py-12 rounded-xl md:rounded-r-none'>
+            <Image src={logo} alt='devoro-logo' className='xl:h-24 xl:w-28 h-16 w-20 mb-10' priority/>
             <div className='flex items-center flex-col gap-5 w-full'>
               <div className='flex relative  w-full'>
                 <div className='flex tems-center gap-4 w-full justify-center'>
-                  <input onChange={handleChange} value={values.email} id="email" name="email" type='text' placeholder='Email' className='w-[50%] px-2 py-2 rounded-lg border border-black/30'/>
+                  <input onChange={handleChange} value={values.email} id="email" name="email" type='text' placeholder='Email' className='xl:w-[50%] w-[80%] px-2 py-2 rounded-lg border border-black/30'/>
                 </div>
-                <div className='absolute right-[107px] top-2'>
+                <div className='absolute xl:right-[107px] right-14 top-2'>
                   {errors.email &&
                   <TooltipProvider>
                     <Tooltip>
                       <TooltipTrigger>
                         <RiErrorWarningFill size={24} className='text-red-800'/>
                       </TooltipTrigger>
-                      <TooltipContent>
-                        <p className='text-white'>{errors.email}</p>
+                      <TooltipContent side='left'>
+                        <p className=' text-white xl:text-base text-[14px]'>{errors.email}</p>
                       </TooltipContent>      
                     </Tooltip>
                   </TooltipProvider>
@@ -93,8 +93,8 @@ export default function SignUp() {
                       <TooltipTrigger>
                         <RiErrorWarningFill size={24} className='text-red-800'/>
                       </TooltipTrigger>
-                      <TooltipContent>
-                        <p className='text-white'>{alreadyUsed}</p>
+                      <TooltipContent side='left'>
+                        <p className='text-white xl:text-base text-[14px]'>{alreadyUsed}</p>
                       </TooltipContent>      
                     </Tooltip>
                   </TooltipProvider>}  
@@ -102,17 +102,17 @@ export default function SignUp() {
               </div>
               <div className='flex relative w-full'>
                 <div className='flex tems-center gap-4 w-full justify-center'>
-                  <input onChange={handleChange} value={values.password} id="password" name="password" type='password' placeholder='Password' className='w-[50%] px-2 py-2 rounded-lg border border-black/30'/>
+                  <input onChange={handleChange} value={values.password} id="password" name="password" type='password' placeholder='Password' className='xl:w-[50%] w-[80%] px-2 py-2 rounded-lg border border-black/30'/>
                 </div>
-                <div className='absolute right-[107px] top-2'>
+                <div className='absolute xl:right-[107px] right-14  top-2'>
                   {errors.password &&
                   <TooltipProvider>
                     <Tooltip>
                       <TooltipTrigger>
                         <RiErrorWarningFill size={24} className='text-red-800'/>
                       </TooltipTrigger>
-                      <TooltipContent>
-                        <p className='text-white'>{errors.password}</p>
+                      <TooltipContent side='left'>
+                        <p className='text-white xl:text-base text-[14px]'>{errors.password}</p>
                       </TooltipContent>      
                     </Tooltip>
                   </TooltipProvider>
@@ -121,17 +121,17 @@ export default function SignUp() {
               </div>
               <div className='flex relative mb-5 w-full'>
                 <div className='flex tems-center gap-4 w-full justify-center'>
-                  <input onChange={handleChange} value={values.cpassword} id="cpassword" name="cpassword" type='password' placeholder='Confirm Password' className='w-[50%] px-2 py-2 rounded-lg border border-black/30'/>
+                  <input onChange={handleChange} value={values.cpassword} id="cpassword" name="cpassword" type='password' placeholder='Confirm Password' className='xl:w-[50%] w-[80%] px-2 py-2 rounded-lg border border-black/30'/>
                 </div>
-                <div className='absolute right-[107px] top-2'>
+                <div className='absolute xl:right-[107px] right-14 top-2'>
                   {errors.cpassword &&
                   <TooltipProvider>
                     <Tooltip>
                       <TooltipTrigger>
                         <RiErrorWarningFill size={24} className='text-red-800'/>
                       </TooltipTrigger>
-                      <TooltipContent>
-                        <p className='text-white'>{errors.cpassword}</p>
+                      <TooltipContent side='left'>
+                        <p className='text-white xl:text-base text-[14px]'>{errors.cpassword}</p>
                       </TooltipContent>      
                     </Tooltip>
                   </TooltipProvider>
@@ -139,8 +139,8 @@ export default function SignUp() {
                 </div>
               </div>
             </div>
-            <div className='flex flex-col items-center text-center gap-5 w-[50%]'>
-              <button type='submit' onClick={handleUserSignup} className='bg-primary py-2 px-24 w-full rounded-md font-semibold hover:bg-red-700 text-white'>Sign up</button>
+            <div className='flex flex-col items-center text-center gap-5 xl:w-[50%] w-[80%]'>
+              <button type='submit' onClick={handleUserSignup} className='bg-primary py-2 xl:px-24 w-full rounded-md font-semibold hover:bg-red-700 text-white'>Sign up</button>
               <div className='flex items-center gap-1 text-sm w-[110%] justify-center'>
                   <span>Already have an account?</span>
                   <Link href={'/login'} className='text-primary underline hover:font-normal'>Login</Link>
